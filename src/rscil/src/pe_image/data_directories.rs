e55044afa1a59be23be6aacc38d876f614a63337
Gargafield <gargafield@outlook.com>
2024-12-18 00:23:26 +0100

    *happy rust noise*

--- src/rscil/src/pe_image/data_directories.rs
+++ src/rscil/src/pe_image/data_directories.rs
@@ -1,4 +1,3 @@
-
 /// # II.25.2.3.3 PE header data directories 
 ///
 /// The optional header data directories give the address and size of several tables that appear in the 
@@ -46,28 +45,29 @@ pub struct DataDirectories {
 }
 
 impl DataDirectories {
-    fn read_pair(slice: &[u8; 8]) -> (u32, u32) {
-        (u32::from_le_bytes(slice[0..4].try_into().unwrap()), u32::from_le_bytes(slice[4..8].try_into().unwrap()))
+    fn read_pair(slice: &[u8], start: usize) -> (u32, u32) {
+        (u32::from_le_bytes(slice[start..start+4].try_into().unwrap()),
+         u32::from_le_bytes(slice[start..start+8].try_into().unwrap()))
     }
 
     pub fn from(slice: &[u8; 128]) -> DataDirectories {
         DataDirectories {
-            export_table: Self::read_pair(slice[0..8].try_into().unwrap()),
-            import_table: Self::read_pair(slice[8..16].try_into().unwrap()),
-            resource_table: Self::read_pair(slice[16..24].try_into().unwrap()),
-            exception_table: Self::read_pair(slice[24..32].try_into().unwrap()),
-            certificate_table: Self::read_pair(slice[32..40].try_into().unwrap()),
-            base_relocation_table: Self::read_pair(slice[40..48].try_into().unwrap()),
-            debug: Self::read_pair(slice[48..56].try_into().unwrap()),
-            copyright: Self::read_pair(slice[56..64].try_into().unwrap()),
-            global_ptr: Self::read_pair(slice[64..72].try_into().unwrap()),
-            tls_table: Self::read_pair(slice[72..80].try_into().unwrap()),
-            load_config_table: Self::read_pair(slice[80..88].try_into().unwrap()),
-            bound_import: Self::read_pair(slice[88..96].try_into().unwrap()),
-            iat: Self::read_pair(slice[96..104].try_into().unwrap()),
-            delay_import_descriptor: Self::read_pair(slice[104..112].try_into().unwrap()),
-            cli_header: Self::read_pair(slice[112..120].try_into().unwrap()),
-            reserved: Self::read_pair(slice[120..128].try_into().unwrap()),
+            export_table: Self::read_pair(slice, 0),
+            import_table: Self::read_pair(slice, 8),
+            resource_table: Self::read_pair(slice, 16),
+            exception_table: Self::read_pair(slice, 24),
+            certificate_table: Self::read_pair(slice, 32),
+            base_relocation_table: Self::read_pair(slice, 40),
+            debug: Self::read_pair(slice, 48),
+            copyright: Self::read_pair(slice, 56),
+            global_ptr: Self::read_pair(slice, 64),
+            tls_table: Self::read_pair(slice, 72),
+            load_config_table: Self::read_pair(slice, 80),
+            bound_import: Self::read_pair(slice, 88),
+            iat: Self::read_pair(slice, 96),
+            delay_import_descriptor: Self::read_pair(slice, 104),
+            cli_header: Self::read_pair(slice, 112),
+            reserved: Self::read_pair(slice, 120),
         }
     }
 }
